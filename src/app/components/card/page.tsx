f4644e2074bd0d5f4482e9fd19f2316d0f493432
GeniferGonzalezSantos <genh_@hotmail.com>
2025-03-26 16:46:20 -0300

    consumption of the api construction of the card component and the people and home pages

--- src/app/components/card/page.tsx
+++ src/app/components/card/page.tsx
@@ -1,36 +1,20 @@
 import React from "react";
 import { CardProps } from "./types";
 
-
-export function Card() {
-  const cards: CardProps[] = [
-    {
-      title: "Characters",
-      description: "87 characters",
-      imageUrl: "/characters"
-    },
-    {
-        title: "Planets",
-        description: "60 planets",
-        imageUrl: "/planets"
-    },
-    {
-        title: "Starships",
-        description: "37 starships",
-        imageUrl: "/starships"
-    },
-  ];
-
+export const Card = <T,>({ items = [], renderItem, title }: CardProps<T>) => {
   return (
-    <div className="grid grid-cols-1 gap-8 sm:grid-cols-3">
-    {cards.map((card) => (
-      <a key={card.title} href={card.imageUrl} className="card">
-        <div className="p-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow">
-          <h2 className="text-lg font-bold">{card.title}</h2>
-          <p className="text-sm text-gray-600">{card.description}</p>
-        </div>
-      </a>
-    ))}
+    <div>
+      {title && <h1 className="text-2xl font-bold mb-4">{title}</h1>}
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-3">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="p-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow"
+          >
+            {renderItem(item)}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Card } from "./components/card/page";
 
@@ -5,14 +6,21 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-Orbitron-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <div className="w-full max-w-[400px]">
-          <input
-            type="search"
-            placeholder="research a character or planet"
-            className="w-full px-4 py-2 text-sm text-gray-800 placeholder-gray-400 bg-gray-100 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          />
-        </div>
-          <Card />
+        <Card
+          items={["All", "Personagem", "Planeta", "Favoritos"]}
+          renderItem={(item: string) => (
+            <div
+              onClick={() => {
+                if (item === "Personagem") {
+                  window.location.href = "/people";
+                }
+              }}
+              className="cursor-pointer"
+            >
+              {item}
+            </div>
+          )}
+        />
       </main>
     </div>
   );
